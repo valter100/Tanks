8a90ca943c1d46e02b3d4a16c26b8b0d3f443fbb
BigUngaBunga <truls.rockstrom.ystad.nu@gmail.com>
2022-05-18 11:11:07 +0200

    Fixat rotationer och fastsättning i kartan
    Dekorationsföremål roteras korrekt när de fäster sig i kartan.

--- Tanks/Assets/Scripts/Map Scripts/MapObject.cs
+++ Tanks/Assets/Scripts/Map Scripts/MapObject.cs
@@ -15,49 +15,43 @@ namespace Tanks
             if (collision.gameObject.tag == "Tank" || collision.gameObject.tag == "Projectile")
                 Destroy(gameObject);
 
-            //if (collision.gameObject.tag == "Map")
-            //    AttachToObject();
+            if (collision.gameObject.tag == "Map")
+                AttachToObject();
         }
 
-        //private void OnCollisionExit(Collision collision)
-        //{
-        //    if (collision.gameObject.tag == "Map")
-        //        DetachFromObject();
-        //}
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.tag == "Map")
+                DetachFromObject();
+        }
 
-        //private void OnCollisionStay(Collision collision)
-        //{
-        //    if (collision.gameObject.tag == "Map")
-        //    {
-        //        if (IsInvoking(nameof(DetachFromObject)))
-        //            CancelInvoke(nameof(DetachFromObject));
-        //        Invoke(nameof(DetachFromObject), 0.1f);
-        //    }
-                
-        //}
+        private void OnCollisionStay(Collision collision)
+        {
+            if (collision.gameObject.tag == "Map")
+            {
+                if (IsInvoking(nameof(DetachFromObject)))
+                    CancelInvoke(nameof(DetachFromObject));
+                Invoke(nameof(DetachFromObject), 0.1f);
+            }
 
-        //private void AttachToObject()
-        //{
-        //    Ray ray = new Ray(transform.position, Vector3.down);
-        //    RaycastHit hit;
-        //    bool hitATarget = Physics.Raycast(ray, out hit);
-        //    if (hitATarget)
-        //    {
-        //        Quaternion rotation;
-        //        if (rotateAroundZ)
-        //            rotation = Quaternion.Euler(Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg, 90, 90);
-        //        else
-        //            rotation = Quaternion.Euler(0, 0, Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg);
-        //        Rigidbody.rotation = Quaternion.Euler(rotation.eulerAngles + new Vector3(90, 0));
+        }
 
-        //        Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-        //    }
-        //}
+        private void AttachToObject()
+        {
+            Ray ray = new Ray(transform.position, Vector3.down);
+            RaycastHit hit;
+            bool hitATarget = Physics.Raycast(ray, out hit);
+            if (hitATarget)
+            {
+                Rigidbody.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(hit.normal.y, hit.normal.x) * Mathf.Rad2Deg);
+                Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            }
+        }
 
-        //private void DetachFromObject()
-        //{
-        //    Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
-        //}
+        private void DetachFromObject()
+        {
+            Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
+        }
     }
 }
 
