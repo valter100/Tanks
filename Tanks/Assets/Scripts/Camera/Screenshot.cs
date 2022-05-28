5f6c57c5ea88031e880cef370e8afdc3916515b6
FilipTrip <filip.trip01@gmail.com>
2022-05-28 15:32:55 +0200

    Implemented screenshots
    Take a screenshot by pressing CTRL + SHIFT + S.

---
+++ Tanks/Assets/Scripts/Camera/Screenshot.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.LeftControl))
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.S))
+            {
+                ScreenCapture.CaptureScreenshot(Application.dataPath.Replace("Assets", "Screenshots/") + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".png");
+            }
+        }
+    }
+}
